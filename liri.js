ea6c2ca55b749f703322bdeebc2edb565e469372
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-10-02 10:38:56 -0500

    movie lookup working

--- liri.js
+++ liri.js
@@ -3,6 +3,42 @@ var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var moment = require('moment');
 var keys = require("./keys.js");
+
+
+// Function to look up movie
+
+var movie = function(){
+var nodeArgs = process.argv;
+
+var movieName = "";
+for (var i = 3; i < nodeArgs.length; i++) {
+  if (i > 3 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  } else {
+    movieName += nodeArgs[i];
+  }
+}
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+axios.get(queryUrl).then(function(response){
+    console.log('------------------------');
+    console.log('Title: ' + response.data.Title);
+    console.log('Year: ' + response.data.Year);
+    console.log('IMDB Rating: ' + response.data.Ratings[0].Value);
+    console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
+    console.log('Country Produced: ' + response.data.Country);
+    console.log('Language: ' + response.data.Language);
+    console.log('Plot: ' + response.data.Plot);
+    console.log('Actors: ' + response.data.Actors);
+    console.log('------------------------');
+})
+.catch(function(error){
+    console.log(error);
+});
+}
+
+// Function to look up song
+
 var spotify = new Spotify(keys.spotify);
 
 var getArtistName = function(artist){
@@ -30,6 +66,9 @@ var runLiri = function(caseData, functionData){
     switch(caseData){
         case 'spotify-this-song':
             getSongName(functionData);
+            break;
+        case 'movie-this':
+            movie(functionData);
             break;
     }
 }
